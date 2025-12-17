cb7df2d54675c8debaf0a8266bcf703c988517f8
MashXP <realsteelphong@gmail.com>
2025-12-17 16:14:29 +0700

    bug: question view doesn't retain

--- script.js
+++ script.js
@@ -202,19 +202,40 @@ function showQuestion() {
     const optionsWrapper = document.createElement('div');
     optionsWrapper.id = 'options-wrapper'; // New wrapper for options
 
+    const userAnswer = userAnswers[currentQuestionIndex];
+
     options.forEach((option, index) => {
         const button = document.createElement('button');
         button.innerHTML = `<span style="font-weight: bold; margin-right: 10px;">${String.fromCharCode(65 + index)}</span> ${option}`;
         button.classList.add('option-btn');
-        if (delayedFeedbackCheckbox.checked && userAnswers[currentQuestionIndex] && userAnswers[currentQuestionIndex].selectedAnswer === option) {
-            button.classList.add('selected');
+        
+        if (userAnswer) {
+            if (delayedFeedbackCheckbox.checked) {
+                if (userAnswer.selectedAnswer === option) {
+                    button.classList.add('selected');
+                }
+            } else {
+                button.disabled = true;
+                if (userAnswer.selectedAnswer === option) {
+                    button.classList.add(userAnswer.isCorrect ? 'correct' : 'incorrect');
+                }
+                if (!userAnswer.isCorrect && option === userAnswer.correctAnswer) {
+                    button.classList.add('correct');
+                }
+            }
         }
+
         button.dataset.index = index;
         button.dataset.option = option; // Store the option text in a dataset attribute
         button.addEventListener('click', () => selectAnswer(button, option));
         optionsWrapper.appendChild(button); // Append to wrapper
     });
     optionsContainer.appendChild(optionsWrapper); // Append wrapper to container
+
+    if (userAnswer && !delayedFeedbackCheckbox.checked) {
+        answerSelected = true;
+        nextButton.classList.remove('hide');
+    }
 
     // Render LaTeX in the entire question container
     renderMathInElement(questionContainer, {
