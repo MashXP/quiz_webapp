8a67db2a27878755e3cd91fadc513919b9b1ea38
MashXP <realsteelphong@gmail.com>
2026-05-16 19:21:42 +0700

    fix link

--- js/state.js
+++ js/state.js
@@ -80,7 +80,7 @@ const recommendedQuizzes = [
     },
     {
         name: 'BK - History of Vietnamese Communist Party (2026)',
-        url: 'https://github.com/MashXP/BK/tree/master/BK_LSD/quiz/plant'
+        url: 'https://github.com/MashXP/BK/tree/master/BK_LSD/quiz'
     }
 ];
 
